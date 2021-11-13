7e05b75e3775a3decd86345a271e211a56fbf6e7
Sevdat <55320710+Sevdat@users.noreply.github.com>
2021-11-13 01:30:11 +0100

    Update program.kt

--- Forentrence/src/program.kt
+++ Forentrence/src/program.kt
@@ -48,8 +48,11 @@
 
 прочитать файл выбрасывать исключение IOException.
 */
-
 fun main() {
+    var x = 0
+    var f = 0
+    var g = 0
+    var r = -1
 
     var name = mutableListOf<String>("Арсенал", "Бавария", "Интер", "Барселона")
     var list = mutableListOf<List<Int>>(
@@ -58,73 +61,140 @@ fun main() {
         listOf(1, 2, 0, 0),
         listOf(3, 8, 0, 0),
     )
-
-    var x = -1
-    var map2 = mutableMapOf<Int, List<Int>>()
-    while (x != list.size - 1) {
-        x = x + 1
-        map2[x] = list[x]
-    }
-
-    var map3 = mapOf<Double, Int?>()
-    var map4 = mapOf<Double, Int?>()
-    var k = 0
-    while (k != list.size) {
-        var newlist5 = mutableMapOf<Double, Int?>()
-        var newlist6 = mutableMapOf<Double, Int?>()
+//дает координаты X и Y в формате двойного числа в первом списке, а во втором списке он выдает обратную координату
+    var norcor = mapOf<Double, Int>()
+    var revnorcor = mapOf<Double, Int>()
+    while (x != list.size) {
+        var givcor = mutableMapOf<Double, Int>()
+        var revgivcor = mutableMapOf<Double, Int>()
         var y = -1
         while (y != list.size - 1) {
             y = y + 1
-            newlist5["$y.$k".toDouble()] = map2[y]?.get(k)
-            newlist6["$k.$y".toDouble()] = map2[y]?.get(k)
-            map3 = map3 + newlist5
-            map4 = map4 + newlist6
+            givcor["$y.$x".toDouble()] = list[y][x]
+            revgivcor["$x.$y".toDouble()] = list[y][x]
+            norcor = norcor + givcor
+            revnorcor = revnorcor + revgivcor
         }
-        k = k + 1;
+        x = x + 1;
     }
-    val map7 = map3.filter { (i, s) -> ((i * 10) % 11) != 0.0 }.toSortedMap()
-    val map8 = map4.filter { (i, s) -> ((i * 10) % 11) != 0.0 }.toSortedMap()
-
-    var listnew = listOf<Pair<Int?, Int?>>()
-    for ((i, e) in map4) {
-        var newlist = listOf(Pair(map7[i], map8[i]))
-        listnew = listnew + newlist
+//Фильтрует совпадающие координаты X и Y, и организует карту от наименьшего индекса до наибольшего индекса.
+    val filcor = norcor.filter { (i, s) -> ((i * 10) % 11) != 0.0 }.toSortedMap()
+    val revfilcor = revnorcor.filter { (i, s) -> ((i * 10) % 11) != 0.0 }.toSortedMap()
+//Соответствует каждому индексу из первого списка и второго списка, потому что числа принадлежат друг другу
+    var pair = listOf<Pair<Int?, Int?>>()
+    for ((i, e) in revnorcor) {
+        var joincor = listOf(Pair(filcor[i], revfilcor[i]))
+        pair = pair + joincor
     }
-    var listinew = listnew.filter { (i, e) -> i != null && e != null }
-
-
-    var listi = mutableListOf<Int>()
-    for ((d, s) in listinew) {
+//отфильтровывает null значения
+    var nullfil = pair.filter { (i, e) -> i != null && e != null }
+//от каждой пары начисляются очки в зависимости от первого номера
+    var point = mutableListOf<Int>()
+    for ((d, s) in nullfil) {
         when {
-            (d.toString().toInt() > s.toString().toInt()) -> listi.add(3)
-            (d.toString().toInt() < s.toString().toInt()) -> listi.add(0)
-            else -> listi.add(1)
+            (d.toString().toInt() > s.toString().toInt()) -> point.add(3)
+            (d.toString().toInt() < s.toString().toInt()) -> point.add(0)
+            else -> point.add(1)
         }
     }
-
-    var f = 0
-    var g = 0
-    var addlist = mutableListOf<Int>()
-    var addlist1 = mutableListOf<Int>()
+//добавляет значения вместе в зависимости от размера списка
+    var unidel = mutableListOf<Int>()
+    var score = mutableListOf<Int>()
     while(g != list.size) {
         g = g + 1
         while (f != (list.size -1) * g) {
-            addlist = (addlist + listi[f]) as MutableList
+            unidel = (unidel + point[f]) as MutableList
             f = f + 1
         }
-        addlist1 = (addlist1 + addlist.sum()) as MutableList<Int>
-        addlist.clear()
+        score = (score + unidel.sum()) as MutableList<Int>
+        unidel.clear()
     }
-
-    var score = mutableMapOf<String, Int>()
-    var d = -1
-    while (d != list.size - 1) {
-        d = d + 1
-        score[name[d]] = addlist1[d]
+//объединяет имена и добавленные баллы вместе
+    var tesco = mutableMapOf<String, Int>()
+    while (r != list.size - 1) {
+        r = r + 1
+        tesco[name[r]] = score[r]
 
     }
-    println(score)
+    println(tesco)
 }
+
+// fun main() {
+
+//     var name = mutableListOf<String>("Арсенал", "Бавария", "Интер", "Барселона")
+//     var list = mutableListOf<List<Int>>(
+//         listOf(0, 4, 1, 1),
+//         listOf(1, 0, 2, 3),
+//         listOf(1, 2, 0, 0),
+//         listOf(3, 8, 0, 0),
+//     )
+
+//     var x = -1
+//     var map2 = mutableMapOf<Int, List<Int>>()
+//     while (x != list.size - 1) {
+//         x = x + 1
+//         map2[x] = list[x]
+//     }
+
+//     var map3 = mapOf<Double, Int?>()
+//     var map4 = mapOf<Double, Int?>()
+//     var k = 0
+//     while (k != list.size) {
+//         var newlist5 = mutableMapOf<Double, Int?>()
+//         var newlist6 = mutableMapOf<Double, Int?>()
+//         var y = -1
+//         while (y != list.size - 1) {
+//             y = y + 1
+//             newlist5["$y.$k".toDouble()] = map2[y]?.get(k)
+//             newlist6["$k.$y".toDouble()] = map2[y]?.get(k)
+//             map3 = map3 + newlist5
+//             map4 = map4 + newlist6
+//         }
+//         k = k + 1;
+//     }
+//     val map7 = map3.filter { (i, s) -> ((i * 10) % 11) != 0.0 }.toSortedMap()
+//     val map8 = map4.filter { (i, s) -> ((i * 10) % 11) != 0.0 }.toSortedMap()
+
+//     var listnew = listOf<Pair<Int?, Int?>>()
+//     for ((i, e) in map4) {
+//         var newlist = listOf(Pair(map7[i], map8[i]))
+//         listnew = listnew + newlist
+//     }
+//     var listinew = listnew.filter { (i, e) -> i != null && e != null }
+
+
+//     var listi = mutableListOf<Int>()
+//     for ((d, s) in listinew) {
+//         when {
+//             (d.toString().toInt() > s.toString().toInt()) -> listi.add(3)
+//             (d.toString().toInt() < s.toString().toInt()) -> listi.add(0)
+//             else -> listi.add(1)
+//         }
+//     }
+
+//     var f = 0
+//     var g = 0
+//     var addlist = mutableListOf<Int>()
+//     var addlist1 = mutableListOf<Int>()
+//     while(g != list.size) {
+//         g = g + 1
+//         while (f != (list.size -1) * g) {
+//             addlist = (addlist + listi[f]) as MutableList
+//             f = f + 1
+//         }
+//         addlist1 = (addlist1 + addlist.sum()) as MutableList<Int>
+//         addlist.clear()
+//     }
+
+//     var score = mutableMapOf<String, Int>()
+//     var d = -1
+//     while (d != list.size - 1) {
+//         d = d + 1
+//         score[name[d]] = addlist1[d]
+
+//     }
+//     println(score)
+// }
 
 // fun main() {
 
